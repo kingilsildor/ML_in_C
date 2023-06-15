04913bd92790ce9012cc24b18f4017888b80bf68
Tycho <tychostam@gmail.com>
2023-06-15 20:44:43 +0200

    added distance

--- euclidean.c
+++ euclidean.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
+#include <math.h>
 
 float train[][4] = {
-    {0,1,0,0},
-    {0,1,1,0},
+    {3,2,0,0},
+    {2,1,1,0},
 };
 
+#define row_count sizeof(train)/sizeof(train[0])
+#define column_count sizeof(train[0]) / sizeof(train[0][0])
+
+float distance(float* array1, float* array2, int column_len) {
+    float distance = 0.0;
+
+    for (size_t i = 0; i < column_len; i++) {
+        distance += pow(array1[i] - array2[i], 2);
+    }
+
+    return sqrt(distance);  
+}
+
 int main(){
-    return 0
+    float distance_array[row_count];
+    // d = sqrt((p1 - q1)**2 + (p2 - q2)**2)
+    for (size_t i = 0; i < row_count; i++){
+        for (size_t j = 0; j < row_count; j++) {
+            if (i > j){
+                distance_array[i] = distance(train[i], train[j], column_count);
+                printf("%f\n", distance_array[i]);
+            }
+        }           
+    }
+    
+    // float d = sqrt(pow((euq[0][1]- euq[1][1]),2));
+    // printf("%f", d);
+
+
+    return 0;
 }
--- knn.c
+++ knn.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <string.h>
 
 #define MAX_LINE_LENGTH 256
@@ -8,7 +9,15 @@
 
 // Array data: [x1, x2, x3, x4, y]
 
-float euclidean_distance()
+float distance(float* array1, float* array2, int column_len) {
+    float distance = 0.0;
+
+    for (size_t i = 0; i < column_len; i++) {
+        distance += pow(array1[i] - array2[i], 2);
+    }
+
+    return sqrt(distance);  
+}
 
 int main() { 
     char line[MAX_LINE_LENGTH];
@@ -52,6 +61,15 @@ int main() {
    
     }
     
+    float distance_array[numRows];
+    // for (size_t i = 0; i < numRows; i++) {
+    //     for (int j = 0; j < numRows; j++) {
+    //         if (i > j){
+    //             // distance_array[i] = distance(data[i], data[j], MAX_NUM_COLUMN);
+    //             printf("%d\n", data[i]);
+    //         }
+    //     }
+    // }
     for (size_t i = 1; i < numRows; i++) {
         for (int j = 0; j < MAX_NUM_COLUMN; j++) {
             printf("%s ", data[i][j]);
