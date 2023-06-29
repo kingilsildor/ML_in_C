efe09e87f599b10267e9fc52f6f7b2f44067c213
Tycho <tychostam@gmail.com>
2023-06-29 20:33:58 +0200

    Changed the float type to int to combat floating point errors. Add a way te skip the header. Fixt a memory issue with the for loop

--- read_file.c
+++ read_file.c
@@ -7,21 +7,39 @@
 #define MAX_NUM_COLUMN 5
 #define MAX_TOKEN_LENGTH 40
 
+
+
 struct Iris_Data{
     /*
     iris_values1 = sepal length (cm)  iris_values2 = sepal width (cm)
     iris_values3 = petal length (cm)  iris_values4 = petal width (cm)
     */
-    float iris_values[MAX_NUM_COLUMN - 1];
+    int iris_values[MAX_NUM_COLUMN - 1];
     char *iris_class;
 };
 
+struct Euclidean_Distance{
+    float distance;
+    char *compared_iris_class;
+};
+
+float distance(float *data_one, float *data_two, int column_size) {
+    float distance = 0.0;
+    const int SQUARED = 2;
+
+    for (size_t i = 0; i < column_size; i++){
+        distance += powf(data_one[i] - data_two[i], SQUARED);
+    }
+
+    return sqrtf(distance); 
+}
+
 int main(){
     char line[MAX_LINE_LENGTH];    
-    struct Iris_Data data[MAX_NUM_ROWS];
+    struct Iris_Data dataset[MAX_LINE_LENGTH];
     char *token;
+    int numRows = 0;
     
-
     // Open file and check if file is valid.
     FILE* file = fopen("data/iris_dataset.csv", "r");
     if (file == NULL) {
@@ -29,44 +47,45 @@ int main(){
         return 1;
     }
 
+    // Skip header
+    fgets(line, sizeof(line), file);   
+    
+
     // Process the lines in the csv file
-    int numRows = 0; 
+    while (fgets(line, sizeof(line), file) != NULL) {   
+        token = strtok(line, ",");        
 
-    while (fgets(line, sizeof(line), file) != NULL) {
-        char *values[MAX_NUM_COLUMN];
         int numValues = 0;
-
-        token = strtok(line, ",");
+        char *csv_values[MAX_NUM_COLUMN];
         while (token != NULL && numValues < MAX_NUM_COLUMN) {          
-            values[numValues] = token;             
-            
-            float temp_float = atof(values[numValues]);
-            data[numRows].iris_values[numValues] = floor(temp_float * 100) / 100;
+            csv_values[numValues] = token;  
 
+            float temp_float_save = atof(csv_values[numValues]);           
+            dataset[numRows].iris_values[numValues] = round(temp_float_save * 100);
+            
             if (numValues == MAX_NUM_COLUMN - 1) {
                 // Clean the string to remove the \n
-                size_t length = strcspn(values[numValues], "\n");
-                values[numValues][length] = '\0'; 
+                size_t length = strcspn(csv_values[numValues], "\n");
+                csv_values[numValues][length] = '\0'; 
                     
-                data[numRows].iris_class = values[numValues];
+                dataset[numRows].iris_class = csv_values[numValues];
             }
 
             numValues++; 
             token = strtok(NULL, ",");
         }
         numRows++;               
-    } 
+    }
 
-    // for (int i = 1; i < 10; i++) {
-    //     printf("Element %d:\n", i);
-    //     printf("iris_values[0]: %.2f\n", data[i].iris_values[0]);
-    //     printf("iris_values[1]: %.2f\n", data[i].iris_values[1]);
-    //     printf("iris_values[2]: %.2f\n", data[i].iris_values[2]);
-    //     printf("iris_values[3]: %.2f\n", data[i].iris_values[3]);
-    //     printf("iris_class: %s\n", data[i].iris_class);
-    // }
-
-    
+    for (size_t i = 0; i < numRows; i++){
+        printf("Element %d:\n", i);
+        printf("iris_values[0]: %d\n", dataset[i].iris_values[0]);
+        printf("iris_values[1]: %d\n", dataset[i].iris_values[1]);
+        printf("iris_values[2]: %d\n", dataset[i].iris_values[2]);
+        printf("iris_values[3]: %d\n", dataset[i].iris_values[3]);
+        printf("iris_class: %s\n\n", dataset[i].iris_class);
+    }
+      
 
     fclose(file);
     return 0;
--- temp_code/temp.c
+++ temp_code/temp.c
@@ -14,45 +14,56 @@ struct Iris_Data{
     char *iris_class;
 };
 
+struct Distance{
+    float distance;
+    char *iris_class_one;
+    char *iris_class_two;
+};
 
-float distance(float *array1, float *array2, int column_len) {
+float distance(float *data_one, float *data_two) {
     float distance = 0.0;
 
-    for (size_t i = 0; i < column_len; i++) {
-        distance += powf(array1[i] - array2[i], 2);
+    for (size_t i = 0; i < 4; i++){
+        distance += powf(data_one[i] - data_two[i], 2);
     }
 
-    return sqrtf(distance);  
+    return sqrtf(distance); 
 }
 
 int main(){
-
+    struct Distance distance_array[MAX_NUM_ROWS];
     struct Iris_Data data[MAX_NUM_ROWS];
     data[0].iris_values[0] = 5.1;
     data[0].iris_values[1] = 3.5;
     data[0].iris_values[2] = 1.4;
     data[0].iris_values[3] = 0.2;
-    data[0].iris_class = 'Iris-setosa';
+    data[0].iris_class = "Iris-setosa";
 
     data[1].iris_values[0] = 4.9;
     data[1].iris_values[1] = 3.0;
     data[1].iris_values[2] = 1.4;
     data[1].iris_values[3] = 0.2;
-    data[1].iris_class = 'Iris-setosa';
+    data[1].iris_class = "Iris-setosa";
 
-    data[2].iris_values[0] = 5.1;
-    data[2].iris_values[1] = 3.5;
-    data[2].iris_values[2] = 1.4;
-    data[2].iris_values[3] = 0.2;
-    data[2].iris_class = 'Iris-setosa';
+    data[2].iris_values[0] = 6.3;
+    data[2].iris_values[1] = 3.3;
+    data[2].iris_values[2] = 6.0;
+    data[2].iris_values[3] = 2.5;
+    data[2].iris_class = "Iris-virginica";
 
 
     float distance_list[3]; 
     for (size_t i = 0; i < 3; i++){
-        for (size_t i = 0; i < 4; i++){
-            
+        for (size_t j = 0; j < 3; j++){
+            if(i == j){
+                break;
+            }
+            distance_array[i].iris_class_one = data[i].iris_class;
+            distance_array[i].iris_class_two = data[j].iris_class;
+            distance_array[i].distance = distance(data[i].iris_values, data[j].iris_values);
+
+            printf("distance: %.2f, from %s to %s\n", distance_array[i].distance, distance_array[i].iris_class_one, distance_array[i].iris_class_two);
         }
-        
     }
     
 }
